35c9c6cc57f892a42bc6b46428a2acec34b30200
Andre <andregpilakis@gmail.com>
2020-01-04 09:55:14 +0000

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(() => {
+
+    $('#searchForm').on('submit', (e) => {
+        let searchText =  $('#searchText').val();
+        getMovies(searchText);
+        e.preventDefault();
+    });
+});
+
+function getMovies(searchText){
+    axios.get('http://www.omdbapi.com?s='+searchText)
+    .then((response) =>{
+
+
+        
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+}
